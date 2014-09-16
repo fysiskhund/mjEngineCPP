e34af9a40c101b0d9c07220861a217db0846362a
fysiskhund <lanjoe9@gmail.com>
2014-09-17 00:37:12 +0200

    Start of multi-layer skybox implementation
    ^^''

--- jni/graphics/mjSkybox.cpp
+++ jni/graphics/mjSkybox.cpp
@@ -8,6 +8,16 @@ mjSkybox::mjSkybox()
 	scale.Set(10,10,10);
 }
 
+void mjSkybox::PushLevel(GLuint texture)
+{
+	levels.push_back(texture);
+}
+void mjSkybox::SetModels(mjModel* boxModel, mjModel* planeModel)
+{
+	this->boxModel = boxModel;
+	this->planeModel = planeModel;
+}
+
 void mjSkybox::SetCameraPos(mjVector3* cameraPos)
 {
 	this->cameraPos = cameraPos;
@@ -25,7 +35,17 @@ void mjSkybox::Draw(std::vector<mjShader*>& shaderList, float* lookAtMatrix, flo
 	// turn off depth buffer
 	glDepthMask(GL_FALSE);
 	glDisable(GL_DEPTH_TEST);
+
+	model = boxModel;
 	mjObject::Draw(shaderList, lookAtMatrix, projectionMatrix);
+
+	model = planeModel;
+	for(int i = 0; i < levels.size(); i++)
+	{
+		mjObject::Draw(shaderList, lookAtMatrix, projectionMatrix);
+	}
+
+
 	glDepthMask(GL_TRUE);
 	glEnable(GL_DEPTH_TEST);
 }
--- jni/graphics/mjSkybox.h
+++ jni/graphics/mjSkybox.h
@@ -1,6 +1,7 @@
 #ifndef MJ_SKYBOX_H
 #define MJ_SKYBOX_H
 
+#include <vector>
 #include "../core/mjObject.h"
 
 namespace mjEngine{
@@ -10,11 +11,21 @@ class mjSkybox: public mjObject
 public:
 
 	mjSkybox();
+
+
 	void SetCameraPos(mjVector3* cameraPos);
+	void SetModels(mjModel* boxModel, mjModel* planeModel);
+	virtual void PushLevel(GLuint texture);
 	virtual void Update(float t_elapsed) override;
 	virtual void Draw(std::vector<mjShader*>& shaderList, float* lookAtMatrix, float* projectionMatrix) override;
+
 private:
 	mjVector3* cameraPos;
+	std::vector<GLuint> levels;
+
+	mjModel* boxModel;
+	mjModel* planeModel;
+
 };
 
 }
