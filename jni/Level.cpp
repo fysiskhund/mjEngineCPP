f3aa6b394a448fa1154393a69921de2e846b9858
fysiskhund <lanjoe9@gmail.com>
2014-09-27 02:16:25 +0200

    Starting with reading entities
    from the level file

--- jni/Level.cpp
+++ jni/Level.cpp
@@ -11,5 +11,23 @@ void Level::LoadFromFile(const char* fileName)
 
 void Level::Load(XMLDocument* doc)
 {
+	XMLHandle docHandle(doc);
+
+
+	XMLElement* entity = docHandle.FirstChildElement("level").FirstChildElement("entities").FirstChild().ToElement();
+
+	while (entity)
+	{
+		mjObject* obj = NULL;
+		if (strncmp(entity->Name(), "player", 200) == 0)
+		{
+			obj = new Character(MJ_AABB);
+			if (entity->FirstChildElement("pos"))
+			{
+
+			}
+		}
+		entity = entity->NextSiblingElement();
+	}
 
 }
--- jni/Level.h
+++ jni/Level.h
@@ -1,8 +1,13 @@
 #ifndef LEVEL_H
 #define LEVEL_H
 
+#include <vector>
 #include "extLibs/tinyxml/tinyxml2.h"
+#include "core/mjObject.h"
+#include "Character.h"
+//#include "Box.h"
 #include "mjVector3.h"
+#include <string.h>
 
 using namespace mjEngine;
 
@@ -11,6 +16,9 @@ using namespace tinyxml2;
 class Level
 {
 public:
+
+	std::vector<mjObject*> entities;
+
 	void LoadFromFile(const char* fileName);
 	void Load(XMLDocument* doc);
 
