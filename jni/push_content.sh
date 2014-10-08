529377ec45e9291b391750f4721144fa6563b60a
fysiskhund <lanjoe9@gmail.com>
2014-10-08 23:44:28 +0200

    ADB related scripts
    Scripts for slap-restarting adb and quickly updating game content

---
+++ jni/push_content.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+adb -s 015d4a8201600007 push -p /sdcard/mjEngineCPP /sdcard/mjEngineCPP
---
+++ jni/restart_adb.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/common/tools/platform-tools/adb kill-server && /common/tools/platform-tools/adb devices
