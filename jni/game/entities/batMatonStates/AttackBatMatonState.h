f836bca9353a593d2d0643af22defc3d242e0c80
fysiskhund <lanjoe9@gmail.com>
2015-01-13 00:53:40 +0100

    moved files to a tidier location
    The internal linking adjustment is still not done.

---
+++ jni/game/entities/batMatonStates/AttackBatMatonState.h
@@ -0,0 +1,18 @@
+#ifndef ATTACKBATMATONSTATE_H
+#define ATTACKBATMATONSTATE_H
+
+#include "../BatBot.h"
+
+class AttackBatMatonState: public BatBot::BatAutomatonState
+{
+    public:
+        AttackBatMatonState(BatBot* bat);
+        virtual void Execute(float t_elapsed) override;
+        //virtual void Reset() override;
+        virtual ~AttackBatMatonState();
+        mjObject* player = NULL;
+    protected:
+    private:
+};
+
+#endif // ATTACKBATMATONSTATE_H
