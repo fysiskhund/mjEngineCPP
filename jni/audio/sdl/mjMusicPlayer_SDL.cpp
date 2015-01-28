a2205fe43cb0f8b818de4a15a9fb4525c3344efb
fysiskhund <lanjoe9@gmail.com>
2015-01-29 00:42:34 +0100

    continuing..

---
+++ jni/audio/sdl/mjMusicPlayer_SDL.cpp
@@ -0,0 +1,29 @@
+
+mjMusicPlayer::mjMusicPlayer()
+{
+    //ctorsdfsd
+}
+
+void mjMusicPlayer::Play()
+{
+
+}
+void mjMusicPlayer::Play(int sampleIndex)
+{
+
+}
+void mjMusicPlayer::Pause()
+{
+
+}
+
+void mjMusicPlayer::Rewind()
+{
+
+}
+
+mjMusicPlayer::~mjMusicPlayer()
+{
+    //dtor
+}
+
