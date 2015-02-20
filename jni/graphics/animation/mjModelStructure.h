aaf3de09a0704da4877c0cd80210418890b93877
fysiskhund <lanjoe9@gmail.com>
2015-02-20 23:50:28 +0100

    continuing with modelstructure

--- jni/graphics/animation/mjModelStructure.h
+++ jni/graphics/animation/mjModelStructure.h
@@ -2,13 +2,19 @@
 #define MJMODELSTRUCTURE_H
 
 #include "../../extLibs/tinyxml/tinyxml2.h"
+#include <vector>
+
+using namespace tinyxml2;
 
 namespace mjEngine{
-using namespace tinyxml2;
+
+enum modelStructureNodeType {MJ_NODE_PUSH, MJ_NODE_POP, MJ_NODE_NOOP, MJ_NODE_RESET };
+
 
 class mjModelStructure
 {
     public:
+		std::vector<modelStructureNodeType> nodes;
         mjModelStructure();
         mjModelStructure(const char* fileName);
         void Load(XMLDocument* doc);
